85282b4270b7746e2d16af308a6ee35e5eb0519b
tusaunyapat <141983396+tusaunyapat@users.noreply.github.com>
2023-11-14 22:13:00 +0700

    test png

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import pic from "srcassetsshiba.jpg";
 
 function Banner() {
   return (
     <div className=" dark:bg-blue text-xl dark:text-white flex bg-white justify-center">
       <div className="bg-white dark:bg-blue w-full md:w-10/12 xl:w-8/12 flex flex-col sm:flex-row justify-between px-20 items-center py-6 sm:py-24 md:py-36">
         <div className="w-32 my-10 sm:w-64 md:w-108 lg:w-5/12">
-          <img src="src\assets\shiba.jpg" alt="SHIBA"></img>
+          <img src={pic} alt="SHIBA"></img>
         </div>
 
         <div className="flex flex-col justify-center text-center sm:text-right items-center sm:items-end  text-navy dark:text-white  md:py-10">
